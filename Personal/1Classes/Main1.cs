8ef1a45849308f460c884a2dc2909df97349d73a
zmoa.dev <zmoa.dev@gmail.com>
2020-08-31 11:36:43 +0200

    first commit

---
+++ Personal/1Classes/Main1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1Classes
+{
+    class Main1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Class1 name = new Class1();
+
+
+        }
+    }
+}
