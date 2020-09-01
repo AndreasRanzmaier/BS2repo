e052f693be79a81a46553550181d89c38143f64d
zmoa.dev <zmoa.dev@gmail.com>
2020-09-01 15:52:57 +0200

    s

--- Personal/3SudokuSolver/Program.cs
+++ Personal/3SudokuSolver/Program.cs
@@ -11,77 +11,104 @@ namespace _3SudokuSolver
         {
             int row1 = 9;
             int collumn1 = 9;
-            int[,] namenListe = new int[row1, collumn1];
+            int[,] SudkokuFeld = new int[row1, collumn1];
 
             //making one 
-            namenListe[0, 0] = 5;
-            namenListe[0, 1] = 3;
-            namenListe[0, 4] = 7;
-            namenListe[1, 0] = 6;
-            namenListe[1, 3] = 1;
-            namenListe[1, 4] = 9;
-            namenListe[1, 5] = 5;
-            namenListe[2, 1] = 9;
-            namenListe[2, 2] = 8;
-            namenListe[2, 7] = 6;
-            namenListe[3, 0] = 8;
-            namenListe[3, 4] = 6;
-            namenListe[3, 8] = 3;
-            namenListe[4, 0] = 4;
-            namenListe[4, 3] = 8;
-            namenListe[4, 5] = 3;
-            namenListe[4, 8] = 1;
-            namenListe[5, 0] = 7;
-            namenListe[5, 4] = 2;
-            namenListe[5, 8] = 6;
-            namenListe[6, 1] = 6;
-            namenListe[6, 6] = 2;
-            namenListe[6, 7] = 8;
-            namenListe[7, 3] = 4;
-            namenListe[7, 4] = 1;
-            namenListe[7, 5] = 9;
-            namenListe[7, 8] = 5;
-            namenListe[8, 4] = 8;
-            namenListe[8, 7] = 7;
-            namenListe[8, 8] = 9;
+            SudkokuFeld[0, 0] = 5;
+            SudkokuFeld[0, 1] = 3;
+            SudkokuFeld[0, 4] = 7;
+            SudkokuFeld[1, 0] = 6;
+            SudkokuFeld[1, 3] = 1;
+            SudkokuFeld[1, 4] = 9;
+            SudkokuFeld[1, 5] = 5;
+            SudkokuFeld[2, 1] = 9;
+            SudkokuFeld[2, 2] = 8;
+            SudkokuFeld[2, 7] = 6;
+            SudkokuFeld[3, 0] = 8;
+            SudkokuFeld[3, 4] = 6;
+            SudkokuFeld[3, 8] = 3;
+            SudkokuFeld[4, 0] = 4;
+            SudkokuFeld[4, 3] = 8;
+            SudkokuFeld[4, 5] = 3;
+            SudkokuFeld[4, 8] = 1;
+            SudkokuFeld[5, 0] = 7;
+            SudkokuFeld[5, 4] = 2;
+            SudkokuFeld[5, 8] = 6;
+            SudkokuFeld[6, 1] = 6;
+            SudkokuFeld[6, 6] = 2;
+            SudkokuFeld[6, 7] = 8;
+            SudkokuFeld[7, 3] = 4;
+            SudkokuFeld[7, 4] = 1;
+            SudkokuFeld[7, 5] = 9;
+            SudkokuFeld[7, 8] = 5;
+            SudkokuFeld[8, 4] = 8;
+            SudkokuFeld[8, 7] = 7;
+            SudkokuFeld[8, 8] = 9;
 
             solve();
 
+
+            display(SudkokuFeld);
             Console.ReadKey();
 
 
-
+            //reccursive backtracking to solve 
             void solve()
             {
-                row1--;
-                collumn1--;
                 for (int i = 0; i < row1; i++)
                 {
                     for (int j = 0; j < collumn1; j++)
                     {
                         for (int n = 1; n < 10; n++)
                         {
-                            if (Possible(row1, collumn1, n))
+                            if (Possible(i, j, n))
                             {
-                                namenListe[row1, collumn1] = n;
+                                SudkokuFeld[i, j] = n;
                                 solve();
-                                namenListe[row1, collumn1] = 0;
-                                return;
+                                SudkokuFeld[i, j] = 0;                                
                             }
                         }
                     }
                 }
             }            
 
+            void display(int[,] Feld)
+            {
+                int t = 1;
+
+                for (int i = 0; i <  row1; i++)
+                {
+                    for (int j = 0; j < collumn1; j++)
+                    {
+                        if (((t-1) % 3 == 0))
+                        {
+                            Console.Write(" | ");
+                        }
+                        Console.Write(Feld[i,j]);
+                        if (t % 9 == 0)
+                        {
+                         
+                            Console.Write("\n");
+                        }
+                        if (t % 27 == 0)
+                        {
+                            Console.Write("------------------\n");
+                        }
+                        t++;
+                    }
+                }
+            }
+
+            //just to check if field would be posible
             bool Possible(int Row, int Collumn, int tryNumber)
             {
                 //cant put a number somewhere if there already is one
-                if (namenListe[Row, Collumn] == 0)
+                if (SudkokuFeld[Row, Collumn] == 0)
                 {
                     //find all the row numbers
                     for (int i = 0; i < row1; i++)
                     {
-                        if (namenListe[i, Row] == tryNumber)
+                        if (SudkokuFeld[i, Row] == tryNumber)
                         {
                             return false;
                         }
@@ -90,7 +117,7 @@ namespace _3SudokuSolver
                     //find all the collumn numbers
                     for (int i = 0; i < collumn1; i++)
                     {
-                        if (namenListe[Collumn, i] == tryNumber)
+                        if (SudkokuFeld[Collumn, i] == tryNumber)
                         {
                             return false;
                         }
@@ -102,58 +129,14 @@ namespace _3SudokuSolver
                     int minx = 0;
                     int miny = 0;
 
-                    if (haus == 1)
-                    {
-                        minx = 0;
-                        miny = 0;
-                    }
-                    else if (haus == 2)
-                    {
-                        minx = 3;
-                        miny = 0;
-                    }
-                    else if (haus == 3)
-                    {
-                        minx = 6;
-                        miny = 0;
-                    }
-                    else if (haus == 4)
-                    {
-                        minx = 0;
-                        miny = 3;
-                    }
-                    else if (haus == 5)
-                    {
-                        minx = 3;
-                        miny = 3;
-                    }
-                    else if (haus == 6)
-                    {
-                        minx = 6;
-                        miny = 3;
-                    }
-                    else if (haus == 7)
-                    {
-                        minx = 0;
-                        miny = 6;
-                    }
-                    else if (haus == 8)
-                    {
-                        minx = 3;
-                        miny = 6;
-                    }
-                    else if (haus == 9)
-                    {
-                        minx = 6;
-                        miny = 6;
-                    }
+                    minxy(haus, out minx, out miny);
 
 
                     for (int x = minx; x < minx + 3; x++)
                     {
                         for (int y = miny; y < miny + 3; y++)
                         {
-                            if (namenListe[x, y] == tryNumber)
+                            if (SudkokuFeld[x, y] == tryNumber)
                             {
                                 return false;
                             }
@@ -162,14 +145,14 @@ namespace _3SudokuSolver
 
                 }
                 else
-                {
-                    Console.WriteLine("allready a number");
+                {                    
                     return false;
                 }
 
                 return true;
             }
 
+            //determinesthe 3x3 Field in wich the field to check is 
             static int determineHaus(int Row, int Collumn)
             {
                 //ausgabe 1-9 je nach "Haus" also 3x3 kasten
@@ -226,7 +209,56 @@ namespace _3SudokuSolver
                     return 0;
                 }
             }
-
+         
+            //checks the haus and sends back its top left corner 
+            static void minxy(int haus, out int minx, out int miny)
+            {
+                if (haus == 1)
+                {
+                    minx = 0;
+                    miny = 0;
+                }
+                else if (haus == 2)
+                {
+                    minx = 3;
+                    miny = 0;
+                }
+                else if (haus == 3)
+                {
+                    minx = 6;
+                    miny = 0;
+                }
+                else if (haus == 4)
+                {
+                    minx = 0;
+                    miny = 3;
+                }
+                else if (haus == 5)
+                {
+                    minx = 3;
+                    miny = 3;
+                }
+                else if (haus == 6)
+                {
+                    minx = 6;
+                    miny = 3;
+                }
+                else if (haus == 7)
+                {
+                    minx = 0;
+                    miny = 6;
+                }
+                else if (haus == 8)
+                {
+                    minx = 3;
+                    miny = 6;
+                }
+                else  //haus = 9
+                {
+                    minx = 6;
+                    miny = 6;
+                }
+            }
         }
     }
 }
