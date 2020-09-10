8552881e392025fb7f244a2508bc01c6273ecc91
zmoa.dev <zmoa.dev@gmail.com>
2020-09-10 08:50:37 +0200

    3

--- School/02Kontrollstrukturen/Program.cs
+++ School/02Kontrollstrukturen/Program.cs
@@ -7,11 +7,30 @@ namespace _02Kontrollstrukturen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(args[0]);
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+
+            //same with while 
+            int j = 0;
+            while (j < args.Length)
+            {
+                Console.WriteLine(args[j]);
+                j++;
+            }
+
+            //same with dowhile
+            int u = 0;
+            do
+            {
+                Console.WriteLine(args[u]);
+                u++;
+            } while (u < args.Length);
+
 
             Console.ReadKey();
-
-
         }
     }
 }
