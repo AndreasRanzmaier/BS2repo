7d250ac563e912e11cbda3702e1fa82bd31f2bf0
zmoa.dev <zmoa.dev@gmail.com>
2020-09-10 08:32:07 +0200

    01

---
+++ School/01Intro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.SetCursorPosition(5, 5);
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
