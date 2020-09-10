6ec3c3de184dce09f6c5db85e196036d911ce7e7
zmoa.dev <zmoa.dev@gmail.com>
2020-09-10 08:36:04 +0200

    2

---
+++ School/02Kontrollstrukturen/Program.cs
@@ -0,0 +1,17 @@
+﻿//Andreas Ranzmaier 10.09
+using System;
+
+namespace _02Kontrollstrukturen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
