ffa93b40b6a877ea9d11494dc4705ffcdf6c1326
zmoa.dev <zmoa.dev@gmail.com>
2020-09-23 08:13:21 +0200

    a

---
+++ School/08Datentypen/Program.cs
@@ -0,0 +1,18 @@
+﻿//Ranzmaier Andreas 23.09.2020
+using System;
+
+namespace _08Datentypen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 15;
+            decimal d = i;
+
+            i = 100;
+            //short s = i;
+            short s = (short)i;
+        }
+    }
+}
