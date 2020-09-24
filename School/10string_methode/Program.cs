afb9b19c25b255ebec77a0486ea634baeb8ef12e
zmoa.dev <zmoa.dev@gmail.com>
2020-09-24 15:17:45 +0200

    StringArbeitsauftrag

--- School/10string_methode/Program.cs
+++ School/10string_methode/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace _10string_methode
 {
@@ -8,11 +10,20 @@ namespace _10string_methode
         {
             string xml = "<schule> <bs2> <2aITL> <schueler>Mayr Hans </schueler> <schueler>Andr Ranz </schueler> </2aITL> </bs2> </schule>";
 
+            string startTag = "<schueler>";
+            getNames(startTag, xml);
+            
+        }
+
+        static void getNames (string startTag, string xml)
+        {
             int startIndex = 0;
             int endIndex = 0;
 
-            string startTag = "<schueler>";
-            string endTag = "</schueler>";
+            static string endTag(string startTag)
+            {
+                return startTag.Insert(1, "/");
+            }
 
             while (startIndex != -1)
             {
@@ -20,7 +31,7 @@ namespace _10string_methode
 
                 if (startIndex != -1)
                 {
-                    endIndex = xml.IndexOf(endTag, startIndex);
+                    endIndex = xml.IndexOf(endTag(startTag), startIndex);
 
 
 
