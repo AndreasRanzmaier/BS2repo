28ad94c82ea0ed9d1271092bc6439a4cb9a04c9b
zmoa.dev <zmoa.dev@gmail.com>
2020-10-02 10:01:28 +0200

    Dictionary

---
+++ School/14ÜbungSublierung/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14ÜbungSublierung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = new string[] { "Hugo", "Franz", "paul", "", "Maw" };
+            foreach (var item in arr)
+            {
+                // T ist ein generischer Datentyp
+                // bei der list sind die Items verteilt im Speicher und zeigen aufeinander 
+                // Dictionary <TKey,TValue>
+                
+            }
+        }
+    }
+}
