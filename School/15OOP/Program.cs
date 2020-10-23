1d28d3f4b6c935fa5ffab009a7a0c246423b63bf
zmoa.dev <zmoa.dev@gmail.com>
2020-10-23 12:51:20 +0200

    asdf

--- School/15OOP/Program.cs
+++ School/15OOP/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 
 namespace _15OOP
 {
@@ -7,16 +6,71 @@ namespace _15OOP
     {
         static void Main(string[] args)
         {
-            Room raum1 = new Room();
-            raum1.width = 9;
-            raum1.height = 6;
+            //Room
+            {
+                Room raum1 = new Room();
+                //raum1.width = 9;
+                //jajaj
+                Room r2 = new Room(4, 3);
+                Console.WriteLine(r2.width);
+            }
+
+            //Person
+            string a = Console.ReadLine();
+            int b = Convert.ToInt32(Console.ReadLine());
+            Person p1 = new Person(a,b);
+
+            Console.WriteLine(p1.VName);
+            Console.WriteLine(p1.Alter);
         }
 
         class Room
         {
-            public int width = 10;
-            public int height = 5;
+            //standardmäßig ist private 
 
+            // im main sichtbar
+            public int width = 0;
+
+            //nicht im main sichtbar 
+            public int height = 0;
+
+            /* konstruktor 1
+             * Default ÜBERSCHREIBEN 
+             * die Schnittselle ist gleich (kein überladen)             
+             */
+            public Room()
+            {
+                width = 15;
+                height = 20;
+            }
+
+            /*
+              ÜBERLADEN 
+             */
+            public Room(int width, int height)
+            {
+                this.width = width;
+                this.height = height;
+            }
+        }
+
+        class Person
+        {
+            public string VName = "";
+            public int Alter = 0;
+
+            public Person()
+            {
+                VName = "Muster";
+                Alter = 18;
+            }
+
+            public Person(string VName, int Alter)
+            {
+                this.VName = VName;
+                this.Alter = Alter;
+
+            }
         }
     }
 }
