96e99f8e257e0c88aa35691b67080409459175d7
zmoa.dev <zmoa.dev@gmail.com>
2020-10-23 12:09:18 +0200

    as

---
+++ School/15OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _15OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room raum1 = new Room();
+            raum1.width = 9;
+            raum1.height = 6;
+        }
+
+        class Room
+        {
+            public int width = 10;
+            public int height = 5;
+
+        }
+    }
+}
