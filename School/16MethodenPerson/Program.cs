6a64b841d5a408294d2a32e22eb058c2a53f5617
zmoa.dev <zmoa.dev@gmail.com>
2020-10-29 12:49:37 +0100

    a

--- School/16MethodenPerson/Program.cs
+++ School/16MethodenPerson/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Ranzmaier Anderas 28.10.2020
 using System;
+using System.Drawing;
 
 namespace _16Methoden
 {
@@ -11,9 +12,14 @@ namespace _16Methoden
             Console.ForegroundColor = ConsoleColor.Black;
             Console.Clear();
 
-            Bsp2();
-            Bsp3();
-
+            //Bsp2();
+            string tmp_jn;
+            do
+            {
+                Bsp3();
+                Console.WriteLine("den selben song nochmal? j | n");
+                tmp_jn = Console.ReadLine();
+            } while (tmp_jn == "j" || tmp_jn == "J");            
         }
 
         static void Bsp2()
@@ -32,45 +38,64 @@ namespace _16Methoden
         static void Bsp3()
         {
             // Bsp 3) neues Protokoll
-            //Array der Klasse Person
+            // Array der Klasse Person
             Console.WriteLine("Wieviele Personen ?: ");
             int tmp_inp = Convert.ToInt32(Console.ReadLine());
 
             Person[] Person3 = new Person[tmp_inp];
 
-            //wenn ungerade default einmal ausführen
+            // wenn ungerade default einmal ausführen
             int i = 0;
             if (tmp_inp % 2 == 1)
             {
-                Person3[1] = new Person();
+                Person3[0] = new Person();
+
+                // nach dem default erzeugen mit der setMembers Methode auffüllen
+
+                BenuzerEingabe(out string tmp_vname, out string tmp_nname, out int tmp_alter);
+                Person3[0].setMembers(tmp_vname, tmp_nname, tmp_alter);
                 i++;
             }
 
+            // danach / wenn gerade alle werte mit benutzereingeben befüllen 
             while (i < tmp_inp)
             {
-                //Benutzereingabe
-                Console.WriteLine("Vorname[" + i + "] = ");
-                string tmp_vname = Console.ReadLine();
-
-                Console.WriteLine("Nachname[" + i + "] = ");
-                string tmp_nname = Console.ReadLine();
-
-                Console.WriteLine("Alter[" + i + "] = ");
-                int tmp_alter = Convert.ToInt32(Console.ReadLine());
-
+                BenuzerEingabe(out string tmp_vname, out string tmp_nname, out int tmp_alter);
 
                 Person3[i] = new Person(tmp_vname, tmp_nname, tmp_alter);
                 i++;
             }
 
+            // c) AUSGABE
             Console.WriteLine("Default " + Person3[i - 1].DefaultCount);
-            Console.WriteLine("Ueberladen " + Person3[i - 1].UeberladenConst);
+            Console.WriteLine("Ueberladen " + Person3[i - 1].UeberladenConst +"\n");
+            Console.WriteLine("Person " + Person3.Length);
+            for (int j = 0; j < Person3.Length; j++)
+            {                
+                Console.WriteLine("Vorname " + Person3[j].Vorname);
+                Console.WriteLine("Nachname " + Person3[j].Nachname);
+                Console.WriteLine("Alter " + Person3[j].Alter + "\n");
+            }
+
+            //void Ausgabe()
+            void BenuzerEingabe (out string tmp_vname, out string tmp_nname, out int tmp_alter)
+            {
+                //Benutzereingabe
+                Console.WriteLine("Vorname[" + i + "] = ");
+                tmp_vname = Console.ReadLine();
+
+                Console.WriteLine("Nachname[" + i + "] = ");
+                tmp_nname = Console.ReadLine();
+
+                Console.WriteLine("Alter[" + i + "] = ");
+                tmp_alter = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 
     class Person
     {
-        // 1) Declaration
+        //DECLARATION
         private string vorname;
         private string nachname;
         private int alter;
@@ -78,6 +103,7 @@ namespace _16Methoden
         static private int defaultConst;
         static private int ueberladenConst;
 
+        //KONSTRUKTOR
         // 2) Default Konstruktor 
         public Person()
         {
@@ -96,12 +122,20 @@ namespace _16Methoden
             ueberladenConst++;
         }
 
-        // 4) Methode Ausgabe
-        public void AusgabeNamen()
+      
+        //GETTER UND SETTER
+
+        // b) Getter und Setter Methode
+        public string Vorname
         {
-            Console.WriteLine("VName = " + vorname);
-            Console.WriteLine("NName = " + nachname);
-            Console.WriteLine("Alter = " + alter);
+            get
+            {
+                return this.vorname;
+            }
+            set
+            {
+                this.vorname = value;
+            }
         }
 
         // 5) Getter und Setter Methode 
@@ -117,6 +151,30 @@ namespace _16Methoden
             }
         }
 
+        // b) Getter und Setter Methode 
+        public int Alter
+        {
+            get
+            {
+                return this.alter;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.alter = value;
+                }
+                else
+                {
+                    this.alter = 1;
+                }
+                
+            }
+        }
+
+
+        //METHODEN        
+        // get Default
         public int DefaultCount
         {
             get
@@ -125,6 +183,7 @@ namespace _16Methoden
             }
         }
 
+        // get Ueberladen
         public int UeberladenConst
         {
             get
@@ -133,5 +192,27 @@ namespace _16Methoden
             }
         }
 
+        // 4) Methode Ausgabe
+        public void AusgabeNamen()
+        {
+            Console.WriteLine("VName = " + vorname);
+            Console.WriteLine("NName = " + nachname);
+            Console.WriteLine("Alter = " + alter);
+        }
+
+        // b) Methode SetMembers 
+        public void setMembers(string vorname, string nachname, int alter)
+        {
+            this.vorname = vorname;
+            this.nachname = nachname;
+            if (alter > 0)
+            {
+                this.alter = alter;
+            }
+            else
+            {
+                this.alter = 1;
+            }
+        }
     }
 }
---
+++ School/17Koordinaten/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace _17Koordinaten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aufruf();
+        }
+
+        
+        static void Aufruf()
+        {
+            // do ja nein abfrage 
+            string tmp_jn;
+            do
+            {
+                Console.WriteLine("Wieviele Punkte wollen sie einlesen?: ");
+                int tmp_length = Convert.ToInt32(Console.ReadLine());
+                Punkt[] punkt1 = new Punkt[tmp_length];
+
+                // EINLESEN for über alle array stellen 
+                for (int i = 0; i < tmp_length; i++)
+                {
+                    punkt1[i] = new Punkt();
+                    punkt1[i].EinlesenKoordinaten();                    
+                }          
+                                   
+                // AUSGABE 
+                for (int j = 0; j < tmp_length; j++)
+                {
+                    punkt1[j].AusgabeKoordinaten();
+                }
+
+                Console.WriteLine("den selben song nochmal? j | n");
+                tmp_jn = Console.ReadLine();
+            } while (tmp_jn == "j" || tmp_jn == "J");
+        }
+    }
+
+    class Punkt
+    {
+        // VARIABLEN 
+        private double _x;
+        private double _y;
+        static private int _durchlauf;
+
+        // KONSTRUKTOREN
+        // default
+        public Punkt()
+        {
+             _x = 0;
+             _y = 0;
+            _durchlauf = 1;
+        }
+
+        // überladener
+        public Punkt(double x, double y)
+        {
+            _x = x;
+            _y = y;
+
+            // ausnahme bei grenzüberschreitung 
+            Grenzwerte(ref x, ref y);
+        }
+
+        // METHODEN
+        public void EinlesenKoordinaten()
+        {
+            UI(out double x, out double y);
+            Grenzwerte(ref x, ref y);
+            _x = x;
+            _y = y;
+
+            void UI(out double x, out double y)
+            {
+                Console.WriteLine("x: ");
+                x = Convert.ToDouble(Console.ReadLine());      
+                Console.WriteLine("y: ");
+                y = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+
+        public void AusgabeKoordinaten()
+        {
+            Console.WriteLine("Koordinate" + _durchlauf +" [" + _x + ", " + _y + "]");
+            _durchlauf++;
+        }
+
+        public void Grenzwerte (ref double x, ref double y)
+        {
+            if (x < 0 || x > 400)
+            {
+                x = 0;
+            }
+            if (y < 0 || y > 400)
+            {
+                _y = 0;
+            }
+        }
+    }
+}
