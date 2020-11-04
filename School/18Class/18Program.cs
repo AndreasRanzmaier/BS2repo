ebba49febc6f28bd4386d78e224b081301c5bdeb
zmoa.dev <zmoa.dev@gmail.com>
2020-11-04 10:01:40 +0100

    ä

---
+++ School/18Class/18Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Xml.Serialization;
+
+namespace _18Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+    }
+
+    class Parent
+    {
+        //DECLARATIONS
+        protected int id;
+        private string name;
+
+        //KONSTRUKTOREN
+        public Parent()
+        {
+            id = 0;
+            name = "Parent";
+            Console.WriteLine("Parent");
+        }
+
+        protected Parent(string name)
+        {
+            this.name = name;
+            Console.WriteLine("Parent Parameter");
+        }
+
+        //GET SET
+
+        //METHODEN
+        public void PrintParent ()
+        {
+            Console.WriteLine(id);
+            Console.WriteLine(name);
+        }
+
+    }
+
+    class Child : Parent()
+    {
+        //DECLARATIONS
+        //KONSTRUKTOREN
+        //GET SET
+        //METHODEN
+
+    }
+}
