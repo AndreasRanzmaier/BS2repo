a1b5a3a7bd5d5a418b789d08fdca2f148d045134
zmoa.dev <zmoa.dev@gmail.com>
2021-01-12 17:44:46 +0100

    Adding all assets

--- Personal/5Tetris/5Tetris.cpp
+++ Personal/5Tetris/5Tetris.cpp
@@ -5,11 +5,53 @@
 #include <iostream>
 using namespace std;
 
-wstring
+wstring tetromino[7];
 
 int main()
 {
-   
+   // create assets 
+	{
+		tetromino[0].append(L"..X.");
+		tetromino[0].append(L"..X.");
+		tetromino[0].append(L"..X.");
+		tetromino[0].append(L"..X.");
+
+		tetromino[1].append(L"..X.");
+		tetromino[1].append(L".XX.");
+		tetromino[1].append(L".X..");
+		tetromino[1].append(L"....");
+
+		tetromino[2].append(L".X..");
+		tetromino[2].append(L".XX.");
+		tetromino[2].append(L"..X.");
+		tetromino[2].append(L"....");
+
+		tetromino[3].append(L"....");
+		tetromino[3].append(L".XX.");
+		tetromino[3].append(L".XX.");
+		tetromino[3].append(L"....");
+
+		tetromino[4].append(L"..X.");
+		tetromino[4].append(L".XX.");
+		tetromino[4].append(L"..X.");
+		tetromino[4].append(L"....");
+
+		tetromino[5].append(L".X..");
+		tetromino[5].append(L".XX.");
+		tetromino[5].append(L".X..");
+		tetromino[5].append(L"....");
+
+		tetromino[6].append(L"..X.");
+		tetromino[6].append(L"..X.");
+		tetromino[6].append(L".XX.");
+		tetromino[6].append(L"....");
+
+		tetromino[7].append(L".X..");
+		tetromino[7].append(L".X..");
+		tetromino[7].append(L".XX.");
+		tetromino[7].append(L"....");
+	}
+
 }
 
 
