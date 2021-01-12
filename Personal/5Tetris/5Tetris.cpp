52d0fa5a15651bc68b8cb5af71a0451cbadef195
zmoa.dev <zmoa.dev@gmail.com>
2021-01-12 17:32:18 +0100

    last commit bevore cleaning u9p

---
+++ Personal/5Tetris/5Tetris.cpp
@@ -0,0 +1,15 @@
+// 5Tetris.cpp
+// Andreas Ranzmaier | 12.01.2021 | javidx9 followalong 
+// Reason is relearning c++ syntax and hopefully a broader understanding of Structure 
+
+#include <iostream>
+using namespace std;
+
+wstring
+
+int main()
+{
+   
+}
+
+
