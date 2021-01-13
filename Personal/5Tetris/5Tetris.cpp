9b5f0821dced6b54d5dcba65ebf80a929b2d0bdf
zmoa.dev <zmoa.dev@gmail.com>
2021-01-13 09:57:13 +0100

    adding screen buffer and first game Loop

--- Personal/5Tetris/5Tetris.cpp
+++ Personal/5Tetris/5Tetris.cpp
@@ -3,12 +3,19 @@
 // Reason is relearning c++ syntax and hopefully a broader understanding of Structure 
 
 #include <iostream>
+#include <Windows.h>
+
+
 using namespace std;
+
 // ----------- Variables -------------
 wstring tetromino[7];
 int nFieldWidth = 12;
 int nFieldHeight = 18;
 unsigned char* pField = nullptr;
+
+int nScreenWidth = 80;		// console Screensize X
+int nScreenHeight = 30;		// console Screensize Y
 
 // ----------- Methodes --------------
 int Rotate(int px, int py, int r);
@@ -16,58 +23,87 @@ int Rotate(int px, int py, int r);
 int main()
 {
 	// create assets / fills the tetromino array
-	{
-		tetromino[0].append(L"..X.");
-		tetromino[0].append(L"..X.");
-		tetromino[0].append(L"..X.");
-		tetromino[0].append(L"..X.");
 
-		tetromino[1].append(L"..X.");
-		tetromino[1].append(L".XX.");
-		tetromino[1].append(L".X..");
-		tetromino[1].append(L"....");
+	tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
 
-		tetromino[2].append(L".X..");
-		tetromino[2].append(L".XX.");
-		tetromino[2].append(L"..X.");
-		tetromino[2].append(L"....");
+	tetromino[1].append(L"..X.");
+	tetromino[1].append(L".XX.");
+	tetromino[1].append(L".X..");
+	tetromino[1].append(L"....");
 
-		tetromino[3].append(L"....");
-		tetromino[3].append(L".XX.");
-		tetromino[3].append(L".XX.");
-		tetromino[3].append(L"....");
+	tetromino[2].append(L".X..");
+	tetromino[2].append(L".XX.");
+	tetromino[2].append(L"..X.");
+	tetromino[2].append(L"....");
 
-		tetromino[4].append(L"..X.");
-		tetromino[4].append(L".XX.");
-		tetromino[4].append(L"..X.");
-		tetromino[4].append(L"....");
+	tetromino[3].append(L"....");
+	tetromino[3].append(L".XX.");
+	tetromino[3].append(L".XX.");
+	tetromino[3].append(L"....");
 
-		tetromino[5].append(L"..X.");
-		tetromino[5].append(L"..X.");
-		tetromino[5].append(L".XX.");
-		tetromino[5].append(L"....");
+	tetromino[4].append(L"..X.");
+	tetromino[4].append(L".XX.");
+	tetromino[4].append(L"..X.");
+	tetromino[4].append(L"....");
 
-		tetromino[6].append(L".X..");
-		tetromino[6].append(L".X..");
-		tetromino[6].append(L".XX.");
-		tetromino[6].append(L"....");
-	}
+	tetromino[5].append(L"..X.");
+	tetromino[5].append(L"..X.");
+	tetromino[5].append(L".XX.");
+	tetromino[5].append(L"....");
+
+	tetromino[6].append(L".X..");
+	tetromino[6].append(L".X..");
+	tetromino[6].append(L".XX.");
+	tetromino[6].append(L"....");
+
 
 	// initialises the playingfield as unsigned chars
+
+	pField = new unsigned char[nFieldWidth * nFieldHeight]; // initialising
+	for (int i = 0; i < nFieldWidth; i++) // setting the boundry 
 	{
-		pField = new unsigned char[nFieldWidth * nFieldHeight];
+		for (int ii = 0; ii < nFieldHeight; ii++)
+		{
+			pField[ii * nFieldHeight + i] = (i == 0 || i == nFieldWidth || ii == nFieldHeight - 1) ? 9 : 0; // filling it with 9 if on the side and 0 if inside
+		}
+	}
+
+
+	// creates screen buffer
+	// replaces cw
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	for (int i = 0; i < nScreenWidth * nScreenHeight; i++)
+	{
+		screen[i] = L' ';
+	}
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+
+
+	// Game Loops
+	bool bGameOver = false;
+
+	while (!bGameOver)
+	{
+		// Draw Field 
+		// Display Frame
 		for (int i = 0; i < nFieldWidth; i++)
 		{
 			for (int ii = 0; ii < nFieldHeight; ii++)
 			{
-				pField[ii * nFieldHeight + i] = (i == 0 || i == nFieldWidth);
+				screen[(ii + 2) * nScreenWidth + (i + 2)] = L" OOOOOOO=#"[pField[ii * nFieldWidth + i]]; // L" OOOOOO" is the Template for the Tiels
 			}
 		}
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
 	}
+
 	int temp = Rotate(4, 4, 3);
 	cout << temp;
-
-
 }
 
 int Rotate(int px, int py, int r)
