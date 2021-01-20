1ac3d3e2072e4b25e67d4fa4cc42fecefd34e1eb
zmoa.dev <zmoa.dev@gmail.com>
2021-01-20 15:57:34 +0100

    more pointers

--- C++/Advanced/3Pointers/3Pointers/3Pointers.cpp
+++ C++/Advanced/3Pointers/3Pointers/3Pointers.cpp
@@ -2,23 +2,35 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-	int lol = 10;
-	// ---------------refferencing a pointer ---------------
-	// int* is a pointer to an integer variable and can only
-	// be given the memory position of said integer
-	// & assigns the position
+	// basics and syntax
+	{
+		int lol = 10;
+		// ---------------refferencing a pointer ---------------
+		// int* is a pointer to an integer variable and can only
+		// be given the memory position of said integer
+		// & assigns the position
 
-	int* ptr = &lol;
-	std::cout << ptr << std::endl;
+		int* ptr = &lol;
+		std::cout << ptr << std::endl;
 
 
-	// --------------- de refferencing a pointer ---------------
-	// getting the integer stuff in the pointer location
+		// --------------- de refferencing a pointer ---------------
+		// getting the integer stuff in the pointer location
 
-	std::cout << *ptr << std::endl;
+		std::cout << *ptr << std::endl;
+
+	}
+	
+	int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	int* arrPtr = arr;
+	cout << *arrPtr << endl; //pointing to 0
+	arrPtr++;
+	cout << *arrPtr << endl; //pointing to 1
 
 
 	system("pause");
