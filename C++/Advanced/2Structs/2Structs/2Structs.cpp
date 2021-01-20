77717bb73286749fa1297ebfb14228c1b37b1cbb
zmoa.dev <zmoa.dev@gmail.com>
2021-01-20 14:39:01 +0100

    more in structs

--- C++/Advanced/2Structs/2Structs/2Structs.cpp
+++ C++/Advanced/2Structs/2Structs/2Structs.cpp
@@ -2,11 +2,40 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
 
+struct Player
+{
+    string DisplayName;
+    int Level;
+    float Health;
+
+    void TakeDamage(float dmg)
+    {
+        Health -= dmg;
+    }
+
+    // alternative way to acces struct variables 
+    int GetLevel()
+    {
+        if (Level > 9)
+        {
+            cout << "Level is allready geater than 9!";
+        }
+        return Level;
+    }
+};
 
 int main()
 {
+    Player Paul;
+    Paul.DisplayName = "Paulstar";
+    Paul.Level = 1;
+    Paul.Health = 100.f;
     
+    cout << "p_1 level= " << Paul.GetLevel() << endl;
+
+    system("pause");
 }
 
