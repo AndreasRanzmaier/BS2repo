f13220bec9e98140e335f4d71a263dabed037ed7
zmoa.dev <zmoa.dev@gmail.com>
2021-01-20 13:37:02 +0100

    adding new Struct project

---
+++ C++/Advanced/2Structs/2Structs/2Structs.cpp
@@ -0,0 +1,12 @@
+// 2Structs.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    
+}
+
