7211a368f8ac5f8e0b4ed384f14cc3bd6a8a53aa
zmoa.dev <zmoa.dev@gmail.com>
2021-02-01 13:33:56 +0100

    Acces Modifyers and more work with getter setter and classes in general

---
+++ C++/Advanced/AccesModifyers/AccesModifyers/AccesModifyers.cpp
@@ -0,0 +1,85 @@
+// Getters and Setters 
+// A way of changing private variables
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Creature
+{
+public:
+	Creature()
+	{
+		cout << "a creature has been created" << endl;
+		Health = 100.f;
+	}
+
+	void SetName(string _Name)
+	{
+		Name = _Name;
+	}
+
+	string GetName()
+	{
+		return Name;
+	}
+
+	float GetHealth()
+	{
+		return Health;
+	}
+
+	void TakeDamage(float damage)
+	{
+		cout << "taking " << damage << " damage!" << endl;
+		float Total;
+		Total = Health - damage;
+		
+		if (Total <= 0.f)
+			cout << GetName() << " has died";
+		else
+			Health -= damage;
+
+		cout << "Health: " << GetHealth() << endl;
+	}
+
+
+private:
+	float Health;
+	string Name;
+
+protected:
+	// only accessible from this and child classes
+	int NumberOfLimbs;
+};
+
+class Goblin : public Creature
+{
+public: 
+	Goblin()
+	{
+		cout << "its a Goblin" << endl;
+		NumberOfLimbs = 5;
+		SetName("Doby");
+	}
+
+
+};
+
+int main()
+{
+	Creature Igor;
+	Igor.SetName("Igor");
+
+	cout << "Name: " << Igor.GetName() << endl;
+	cout << "Health: " << Igor.GetHealth() << endl;
+	Igor.TakeDamage(34);
+
+
+	Goblin Gobby;
+	cout << Gobby.GetName() << endl;
+
+
+	system("pause");
+}
+
--- C++/Advanced/Classes_Inheritance/Classes_Inheritance/Classes_Inheritance_more.cpp
+++ C++/Advanced/Classes_Inheritance/Classes_Inheritance/Classes_Inheritance_more.cpp
@@ -16,11 +16,13 @@ public:
 		Limbs = 4;
 	}
 
-	// overloded
-	Animal(string _Name, int _Age, int _Limbs)
-	{
+	// overloded Constructor
+	Animal(string _Name, int _Age, int _Limbs) : Name(_Name), Age(_Age), Limbs(_Limbs) { Report(); }
 
-	}
+	//same as the Notation Above
+	//Name = _Name;
+	//Age = _Age;
+	//Limbs = _Limbs;
 
 	// Variables
 	int Age;
@@ -30,17 +32,46 @@ public:
 	// Methodes 
 	void Report()
 	{
+		cout << endl;
 		cout << "Limbs:" << Limbs << endl;
 		cout << "Name: " << Name << endl;
 		cout << "Age:" << Age << endl;
+		cout << endl;
 	}
 };
 
-int main()
+class Dog : public Animal
 {
-	Animal Bob;
-	Bob.Report();
+	// even tough the Animal class is all public 
+	// Dog is private by default 
+public:
+	Dog() { cout << "it's a dog named: " << Name <<endl; };
+	
+	//calling the allready existing overloadeded constructor from parent 
+	Dog(string _Name, int _Age, int _Limbs) 
+	{ 
+		// calling the allready existing one 
+		//Animal(_Name, _Age, _Limbs);
+
+		// or creating a new one 
+		Name = _Name;
+		Age = _Age;
+		Limbs = _Limbs;
+
+		Report();
+	}
+
+	void speak()
+	{
+		cout << "wuff" << endl;
+	}
+};
+
+int main() 
+{
+	Dog Bello("Bello", 4, 5);
+	Bello.speak();
 
 	system("pause");
-}
+ }
 
